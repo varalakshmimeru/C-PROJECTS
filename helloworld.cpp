ec29274a89510129913c71f07677c7aa9ba94a0f
varalakshmimeru <varalakshmi.meruva9999@gmail.com>
2025-03-29 10:47:27 -0500

    Add files via upload

---
+++ helloworld.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+int main() {
+//integer wholenumber
+int age = 21;
+int year = 2024;
+int days = 7.5;
+int month = 8;
+std::cout << year << '\n';
+
+//double including decimal
+double price = 22.53;
+double gpa = 72.56;
+double marks = 100;
+double tempearature = 80.30;
+std::cout << marks << '\n';
+
+//single character
+char grade = 'A';
+char sex = 'F';
+char initial = 'M';
+std::cout << sex ;
+}
